c1fd0ccd952dd3e7cad778df57026832e8e13332
StathisVotsis <stathisvotsis@gmail.com>
2017-10-23 16:40:40 +0300

    asd

---
+++ editroles/editroles.Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace editroles.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be empty")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be empty")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "This field cannot be empty")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+
+        public string Email { get; set; }
+    }
+}
---
+++ editroles/editroles/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="editroles.MvcApplication" Language="C#" %>
